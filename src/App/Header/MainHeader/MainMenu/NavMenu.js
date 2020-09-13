44e7c817494c2f7b9907e5189d3c422a305385aa
mr.funkster <mr.funksters@gmail.com>
2020-09-13 13:15:57 +0300

    Added active status to navbar

--- src/App/Header/MainHeader/MainMenu/NavMenu.js
+++ src/App/Header/MainHeader/MainMenu/NavMenu.js
@@ -1,24 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import NavMenuData from '../../../../common/data/NavMenuData'
 import NavMenuItem from './NavMenuItem'
 
-const NavMenu = () => {
-    return (
-        <ul>
-            {
-                NavMenuData.map(({
-                    name,
-                    link
-                })=>(
-                    <NavMenuItem key = {name} 
-                        name = {name}
-                        link = {link}
-                    />
-                ))
-            }
-        </ul>
-    )
+
+class NavMenu extends Component {
+
+    state = {
+        isActive: {}
+    }
+
+    isActiveObject = () => {
+        let objArr = NavMenuData.map(({name}, index) => (
+            (index === 0) ? {[name]: true} : {[name]: false}
+        ))
+        let isClickedObj = {}
+        for (let i = 0; i < objArr.length; i++) {
+            Object.assign(isClickedObj, objArr[i])
+        }
+        this.setState({
+            isActive: isClickedObj
+        })
+    }
+
+    markActive = (name) => {
+        let clickedObj  = this.state.isActive
+        for (let key in clickedObj) {
+            clickedObj[key] = false
+        }
+        clickedObj[name] = true;
+        this.setState({
+            isActive: clickedObj
+        })
+    }
+
+    componentDidMount() {
+        this.isActiveObject()
+    }
+    
+
+    render() {
+        return (
+            <ul> 
+                {
+                    NavMenuData.map(({name})=>(
+                        <NavMenuItem key = {name} 
+                            name = {name}
+                            isActive = {this.state.isActive}
+                            markActive = {this.markActive}
+                        />
+                    ))
+                }
+            </ul>
+        )
+    }
 }
 
 export default NavMenu
--- src/App/Header/MainHeader/MainMenu/NavMenuItem.js
+++ src/App/Header/MainHeader/MainMenu/NavMenuItem.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import { linkNameParser } from '../../../../common/data/Functions'
 
-const NavMenuItem = ({
-    name,
-    link
-}) => {
-    return (
-        <li>
-            <Link to={link}>{name}</Link>
-        </li>
-    )
+
+class NavMenuItem extends Component {
+
+    state = {
+        isClicked: false
+    }
+
+    navMenuClick = () => {
+        this.setState({
+            isClicked: true
+        })
+    }
+
+    render() {
+        const {
+            name,
+            isActive,
+            markActive
+        } = this.props
+        return (
+            <li className={isActive[name] ? "active" : ""}>
+                <Link onClick={()=>markActive(name)} to={linkNameParser(name)==="home" ? "/" : `/${linkNameParser(name)}`}>{name}</Link>
+            </li>
+        )
+    }
 }
 
 export default NavMenuItem
