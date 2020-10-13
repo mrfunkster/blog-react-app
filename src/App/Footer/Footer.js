c65b215346d4efd558a7d82ac67aa030b3f4e8d0
mr.funkster <mr.funksters@gmail.com>
2020-10-13 17:32:14 +0300

    Added Footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import NavMenu from '../Header/MainHeader/MainMenu/NavMenu'
+import SignLanguage from '../Header/TopMenu/SignLanguage/SignLanguage'
 
 import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <section className="footer">
+            <div className="container">
+                <div className="row footer-row">
+                    <div className="nav-menu">
+                        <NavMenu />
+                    </div>
+                    <div className="search">
+                        <input type="search" placeholder="Search"/>
+                    </div>
+                    <div className="sign-language">
+                        <SignLanguage />
+                    </div>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
--- src/App/Main/MainImages/MainImages.js
+++ src/App/Main/MainImages/MainImages.js
@@ -4,28 +4,28 @@ const MainImages = () => {
     return (
         <section className="main-images">
             <div className="image-container">
-                <img src="images/main-images-1.png" alt=""/>
+                <img src="/images/content/main-images-1.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-2.png" alt=""/>
+                <img src="/images/content/main-images-2.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-3.png" alt=""/>
+                <img src="/images/content/main-images-3.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-4.png" alt=""/>
+                <img src="/images/content/main-images-4.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-5.png" alt=""/>
+                <img src="/images/content/main-images-5.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-6.png" alt=""/>
+                <img src="/images/content/main-images-6.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-7.png" alt=""/>
+                <img src="/images/content/main-images-7.png" alt=""/>
             </div>
             <div className="image-container">
-                <img src="images/main-images-8.png" alt=""/>
+                <img src="/images/content/main-images-8.png" alt=""/>
             </div>
         </section>
     )
